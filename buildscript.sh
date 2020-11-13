f2d8689bc0b24ba96b144b50bd8c74334142285b
Philipp Regling <philippregling@googlemail.com>
2020-11-13 17:31:18 +0100

    Update nginx conf

--- buildscript.sh
+++ buildscript.sh
@@ -1,5 +1,5 @@
 docker build -t pregling/portfolio .
-docker tag pregling/portfolio pregling/portfolio:0.1.0
+docker tag pregling/portfolio pregling/portfolio:0.1.1
 docker tag pregling/portfolio pregling/portfolio:latest
 docker push pregling/portfolio:latest
-docker push pregling/portfolio:0.1.0
+docker push pregling/portfolio:0.1.1
